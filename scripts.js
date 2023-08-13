9968cb1688a64a91b2a34fedaaf1e6bca6593346
Ultra Lord <haxutoasty@gmail.com>
2023-08-13 17:58:51 -0500

    Added Arrow Functions exercise 15.3

---
+++ scripts.js
@@ -0,0 +1,15 @@
+// // Refactor the above code to use two arrow functions. Turn it into a one-liner.
+// const double = (arr) => {return arr.map((val) => {return val * 2})};
+// let fakeArr = [1, 2, 3, 4, 5,];
+
+// // Replace ALL functions with arrow functions:
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+// const squareAndFindEvens = (numbers) => {
+//     let squares = numbers.map((num) => {
+//         return num ** 2;
+//     });
+//     let evens = squares.filter((square) => {
+//         return square % 2 === 0;
+//     });
+//     return evens;
+// }
